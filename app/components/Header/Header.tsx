356d4c0a66698274a401de87910c3df95ffc09e3
ThunderSkl <148318484+ThunderSkl@users.noreply.github.com>
2025-02-19 14:02:43 +0100

    header-links-update

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -6,17 +6,20 @@ function Header() {
     <header className="bg-red-700 text-white p-4 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-3xl font-bold tracking-wide">
-          <img src="app\components\Header\ImgHeader\Marvel_Logo.png" alt="Marvel" width="10%"></img>
+          <img src="app\components\Header\ImgHeader\Marvel_Logo.png" alt="Marvel" width="13%"></img>
         </Link>
-        <nav className="flex gap-6 text-lg">
-          <Link to="/characters" className="hover:text-gray-300">
-            Personajes
+        <nav className="flex gap-5 text-lg">
+          <Link to="/characters" className="px-4 py-2 rounded-lg transition duration-300 hover:bg-red-800">
+            Characters
           </Link>
-          <Link to="/comics" className="hover:text-gray-300">
-            Cómics
+          <Link to="/comics" className="px-4 py-2 rounded-lg transition duration-300 hover:bg-red-800">
+            Comics
           </Link>
-          <Link to="/events" className="hover:text-gray-300">
-            Eventos
+          <Link to="/films" className="px-4 py-2 rounded-lg transition duration-300 hover:bg-red-800">
+            Films
+          </Link>
+          <Link to="/tv-shows" className="whitespace-nowrap px-3 py-2 rounded-lg transition duration-300 hover:bg-red-800">
+            TV Shows
           </Link>
         </nav>
       </div>
