6da07a0fa594c8da56f9b03b617c362cc2996462
dmelero1 <danielmm1103@gmail.com>
2025-02-21 12:29:28 +0100

    Marvel interfaces

---
+++ app/types/interfaces.ts
@@ -0,0 +1,67 @@
+export interface Thumbnail {
+    path: string;
+    extension: string;
+}
+
+export interface ComicSummary {
+    resourceURI: string;
+    name: string;
+}
+
+export interface Comics {
+    available: number;
+    collectionURI: string;
+    items: ComicSummary[];
+    returned: number;
+}
+
+export interface SeriesSummary {
+    resourceURI: string;
+    name: string;
+}
+
+export interface Series {
+    available: number;
+    collectionURI: string;
+    items: SeriesSummary[];
+    returned: number;
+}
+
+export interface Stories {
+    available: number;
+    collectionURI: string;
+    items: any[];
+    returned: number;
+}
+
+export interface Events {
+    available: number;
+    collectionURI: string;
+    items: any[];
+    returned: number;
+}
+
+export interface Character {
+    id: number;
+    name: string;
+    description: string;
+    modified: string;
+    resourceURI: string;
+    thumbnail: Thumbnail;
+    comics: Comics;
+    series: Series;
+    stories: Stories;
+    events: Events;
+}
+
+export interface ApiResponse {
+    code: number;
+    status: string;
+    data: {
+        offset: number;
+        limit: number;
+        total: number;
+        count: number;
+        results: Character[];
+    };
+}
